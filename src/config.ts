2a8a924514296e844939344a6a5a6d381a1826c9
Travis Thomsen <travis.n.thomsen@gmail.com>
2024-01-23 19:08:14 -0600

    fix env var

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@ export const db = process.env.DATABASE || '';
 
 export const url = process.env.RABBITMQ_URL || 'localhost';
 export const username = process.env.RABBITMQ_USER || '';
-export const password = process.env.RABBITMC_PASSWORD || '';
+export const password = process.env.RABBITMQ_PASSWORD || '';
 export const queue = process.env.RABBITMQ_INVENTORY_QUEUE || 'IventoryUpdateQueue';
