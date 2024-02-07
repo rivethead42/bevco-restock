3239ee801f7d3ed1ceeecf29db7bb3e268c2da62
Travis Thomsen <travis.n.thomsen@gmail.com>
2024-02-07 14:10:21 -0600

    remove commented out code

--- src/routes/update.ts
+++ src/routes/update.ts
@@ -2,22 +2,12 @@ import express, { Request, Response } from 'express';
 import { Restock } from '../models/restock';
 import { sendNotification } from '../messaging/notification';
 
-//import { body, validationResult } from 'express-validator';
-//import { Inventory } from '../models/inventory';
-//import { RequestValidationError } from '../errors/request-validation-errors';
-
 const router = express.Router();
 
 router.put(
     '/api/restock/:id/updatestock',
     async (req: Request, res: Response) => {
       const { id } = req.params;
-      //const errors = validationResult(req);
-
-      /*if (!errors.isEmpty()) {
-          console.log(errors.array())
-          //throw new RequestValidationError(errors.array());
-      }*/
 
       const restock = await Restock.findById({ _id: id });
 
